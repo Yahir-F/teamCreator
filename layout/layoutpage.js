e43390de780b2af6cbcb92577c96980d1618e5e8
Yahir Federico <107011404+Yahir-F@users.noreply.github.com>
2022-07-21 22:30:02 -0700

    created boxes to hold information for manager and Engineer

--- layout/layoutpage.js
+++ layout/layoutpage.js
@@ -18,8 +18,25 @@ const loadTeam = (load) => {
         </div>
         `;
     };
+        const createEngineer = (engineer) => {
+            return `
+            <div class="card" employee info>
+            <div class="card-header">
+            <h2 class="title">${engineer.empName()}</h2>
+            <h3 class= "title><i class="fas fa-glasses"></i> ${engineer.empPosition()}</h3>
+            </div>
+            <div class="empBody">
+            <ul class="empInfo">
+                        <li class="info-list"> Id: ${engineer.empId()}</li>
+                        <li class="info-list"> Email: ${engineer.empEmail()}</li>
+                        <li class="info-list"> Github: ${engineer.github}</li>
+                        </ul>
+            </div>
+            </div>
+            `;
+        }
 
-    
+
 
 
 
