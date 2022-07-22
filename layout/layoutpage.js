30f130798f0a70dc1e8224eafc0a27d8ef294aa1
Yahir Federico <107011404+Yahir-F@users.noreply.github.com>
2022-07-21 22:41:45 -0700

    added same layout of code for intern now

--- layout/layoutpage.js
+++ layout/layoutpage.js
@@ -12,7 +12,7 @@ const loadTeam = (load) => {
         <ul class="empInfo">
                  <li class="info-list"> Id: ${manager.empId()}</li>
                  <li class="info-list"> Email: ${manager.empEmail()}</li>
-                <li class="info-list"> Number of Office: ${manager.officeNumber}</li>
+                <li class="info-list"> Number of Office: ${manager.grabOfficeNumber()}</li>
              </ul>
          </div>
         </div>
@@ -29,18 +29,31 @@ const loadTeam = (load) => {
             <ul class="empInfo">
                         <li class="info-list"> Id: ${engineer.empId()}</li>
                         <li class="info-list"> Email: ${engineer.empEmail()}</li>
-                        <li class="info-list"> Github: ${engineer.github}</li>
+                        <li class="info-list"> Github: ${engineer.empGithub()}</li>
                         </ul>
             </div>
             </div>
             `;
         }
 
+        const createIntern = (intern) => {
+            return `
+            <div class="card" employee info>
+            <div class="card-header">
+            <h2 class="title">${intern.empName()}</h2>
+            <h3 class= "title><i class="fas fa-user-graduate"></i> ${intern.empPosition()}</h3>
+            </div>
+            <div class="empBody">
+            <ul class="empInfo">
+                        <li class="info-list"> Id: ${intern.empId()}</li>
+                        <li class="info-list"> Email: ${intern.empEmail()}</li>
+                        <li class="info-list"> School: ${intern.grabSchool()}</li>
+                        </ul>
+            </div>
+            </div>
+            `;
+        }
 
-
-
-
-        <h3 class="title">${manager.empId()}</h3>
 
         
         
