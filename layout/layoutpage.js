8cb0507eaed606151ffd98818207b01e94b8d72d
Yahir Federico <107011404+Yahir-F@users.noreply.github.com>
2022-07-21 22:23:04 -0700

    created manager load pg

---
+++ layout/layoutpage.js
@@ -0,0 +1,43 @@
+
+const loadTeam = (load) => {
+    const createManager = (manager) => {
+        return `
+        
+        <div class="card" employee info>
+        <div class="card-header">
+        <h2 class="title">${manager.empName()}</h2>
+        <h3 class= "title><i class="fas fa-mug-hot"></i> ${manager.empPosition()}</h3>
+        </div>
+        <div class="empBody">
+        <ul class="empInfo">
+                 <li class="info-list"> Id: ${manager.empId()}</li>
+                 <li class="info-list"> Email: ${manager.empEmail()}</li>
+                <li class="info-list"> Number of Office: ${manager.officeNumber}</li>
+             </ul>
+         </div>
+        </div>
+        `;
+    };
+
+    
+
+
+
+        <h3 class="title">${manager.empId()}</h3>
+
+        
+        
+        
+        
+        
+        
+        
+
+
+
+
+
+
+
+
+
