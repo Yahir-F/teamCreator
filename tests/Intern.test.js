36edd6a96a5362b97ce3681b81eabb36557d6638
Yahir Federico <107011404+Yahir-F@users.noreply.github.com>
2022-07-21 20:05:17 -0700

    created intern test

--- tests/Intern.test.js
+++ tests/Intern.test.js
@@ -1,3 +1,9 @@
 const Intern = require("../lib/Intern");
 
-test("Can get school using ")
+test("Can get school using  grabSchool()", () => {
+    const emp = "Harvard";
+    const intern = new Intern("the", "1", "placeholder@gmail.com", emp);
+    expect(intern.grabSchool()).toBe(emp);
+});
+
+
