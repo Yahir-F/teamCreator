679a58f742eeeba6d3acb80c0699d06f29d114e5
Yahir Federico <107011404+Yahir-F@users.noreply.github.com>
2022-07-21 17:33:12 -0700

    made employe file

--- index.js
+++ index.js
@@ -2,10 +2,10 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 
-// //modules lib
-// const Engineer = require("./lib/Engineer");
-// const Intern = require("./lib/Intern");
-// const Manager = require("./lib/Manager');
+//modules lib
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+const Manager = require('./lib/Manager');
 
 const employeeData = [];
 
